cbcf83eede738951928922579427a6530d26ab18
Carlos Cebrian <carlosjesus.cebrian@alu.uclm.es>
2018-03-09 13:26:06 +0100

    Modified run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from flask import Flask,make_response,jsonify,render_template
-import os
 app=Flask(__name__, instance_relative_config=True)
 
 
@@ -8,6 +7,4 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True,host='0.0.0.0', port=port)	
+	app.run()	
