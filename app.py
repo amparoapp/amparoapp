fbd0f9d0ced76778e18c340c68b22ac491f235d1
Carlos Cebrian <carlosjesus.cebrian@alu.uclm.es>
2018-03-09 14:19:35 +0100

    Updated

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask,make_response,jsonify,render_template
-import os
 
 app=Flask(__name__, instance_relative_config=True)
 
@@ -9,5 +8,4 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True, port=41005)
+	app.run(debug=True, use_reloader=True)
