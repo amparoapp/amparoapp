21f432ed0724f348a7c4e77b00087bcc86940e1b
Carlos Cebrian <carlosjesus.cebrian@alu.uclm.es>
2018-03-09 13:23:10 +0100

    Fourth commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask,make_response,jsonify,render_template
-
+import os
 app=Flask(__name__, instance_relative_config=True)
 
 
@@ -10,4 +10,4 @@ def index():
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True,host='0.0.0.0', port=port)
+	app.run(debug=True,host='0.0.0.0', port=port)	
