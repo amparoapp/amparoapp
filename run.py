bb14857741147ae7c8ea9da44cec7e48001053f6
Carlos Cebrian <carlosjesus.cebrian@alu.uclm.es>
2018-03-09 13:20:13 +0100

    Third commit

--- run.py
+++ run.py
@@ -6,3 +6,8 @@ app=Flask(__name__, instance_relative_config=True)
 @app.route('/',methods=['GET'])
 def index():
 	return render_template('index.html')
+
+if __name__ == '__main__':
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True,host='0.0.0.0', port=port)
